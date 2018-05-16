0ed668c43da1d090a9dc3fff1bc5317a7603ff2c
inh556 <yanbin.jin@gmail.com>
2018-05-16 16:55:54 -0700

    created server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 @Injectable({
   providedIn: 'root'
 })
-export class HeroService {
 
-  constructor() { }
+export class HeroService {
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
+  constructor(private messageService: MessageService) { }
 }
