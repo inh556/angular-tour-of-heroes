4a8b891aea13146fd3acd76d891dbb6a500c803a
inh556 <yanbin.jin@gmail.com>
2018-05-17 17:11:33 -0700

    added annotation for routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent }      from './heroes/heroes.component';
 
 const routes: Routes = [
+  // path: a string that matches the URL in the browser address bar.
+  // component: the component that the router should create when navigating to this route.
   { path: 'heroes', component: HeroesComponent }
 ];
 
